4d5774299692171a6669b1a92588b726203c3e42
ShehzadAslamOza <shehzadaslamoza@gmail.com>
2024-10-01 10:52:17 +0500

    fetched data and rendered the list

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,40 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
+}
+
+class Post {
+  final int id;
+  final String slug;
+  final String url;
+  final String title;
+  final String content;
+  final String image;
+
+  Post({
+    required this.id,
+    required this.slug,
+    required this.url,
+    required this.title,
+    required this.content,
+    required this.image
+  });
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      id: json['id'],
+      slug: json['slug'],
+      url: json['url'],
+      title: json['title'],
+      content: json['content'],
+      image: json['image']
+    );
+  }
+
 }
 
 class MyApp extends StatelessWidget {
@@ -31,95 +64,89 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: _Random(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class _Random extends StatelessWidget {
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  Future<List<Post>> fetchAllPosts() async {
+    final response = await http.get(Uri.parse('http://jsonplaceholder.org/posts'));
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+    if (response.statusCode == 200) {
+      List jsonResponse = jsonDecode(response.body);
+      return jsonResponse.map((post) => Post.fromJson(post)).toList();
+    } else {
+      throw Exception("Failed to load posts");
+    }
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
   }
+
+  
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+     return Scaffold(
+      appBar: AppBar(title: const Text('hwhwh YOYO')),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        child: FutureBuilder(future: fetchAllPosts(), builder: (context,snap) {
+          if (snap.hasData) {
+            return ListView.builder(
+                itemCount: snap.data?.length,
+                itemBuilder: (BuildContext context, int index) {
+                return Column(
+                  children: <Widget> [
+                   Card(
+                       child: ListTile(
+                        leading: CircleAvatar(
+                          child: Image.network('${snap.data?[index].image}')),
+                        title: Text('${snap.data?[index].title}'),
+                        subtitle:
+                        Text('${snap.data?[index].content}'),
+                        trailing: Icon(Icons.more_vert),
+                        isThreeLine: true,
+                  ),
+                ),
+                Divider()
+              ],
+            );
+           
+          },
+        );
+          }
+          else if (snap.hasError) {
+            return Text('error in fetch');
+          }
+          return Center(child: CircularProgressIndicator(),);
+        }),
+      )
+    );
+  }
+}
+
+
+class _MyHomePageState extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('hwhwh YOYO')),
+      body: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (BuildContext context, int index) {
+            return Column(
+              children: <Widget> [
+                ListTile(
+                  title:  Text('Item $index'),
+                ),
+                Divider()
+              ],
+            );
+           
+          },
+        )
     );
   }
 }
